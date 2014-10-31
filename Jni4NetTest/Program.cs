4125975055413bf6a52312efe2d5d28986b011d6
Loc Nguyen <locnguyen.sp@gmail.com>
2014-10-31 00:47:35 -0400

    first commit

---
+++ Jni4NetTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+using net.sf.jni4net;
+using net.sf.jni4net.adaptors;
+using com.skipper.test;
+
+namespace Jni4NetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setup = new BridgeSetup();
+            setup.Verbose = true;
+            setup.AddAllJarsClassPath("./");
+
+            Bridge.CreateJVM(setup);
+            Bridge.RegisterAssembly(typeof(Test).Assembly);
+
+            Test test = new Test();
+            test.hello();
+            test.repeat("It works!");
+
+            Console.WriteLine("\nPress any key to quit.");
+            Console.ReadKey();
+
+        }
+    }
+}
